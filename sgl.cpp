0105ed7c5b96b7d54e18e39c0f055989b1773b0c
lazarjan <unconfigured@null.spigotmc.org>
2020-10-12 20:56:44 +0200

    My first commit

--- sgl.cpp
+++ sgl.cpp
@@ -17,17 +17,78 @@ static sglEErrorCode _libStatus = SGL_NO_ERROR;
 
 /// Other structures
 struct colorPixel {
-	int r;
-	int g;
-	int b;
+	float r;
+	float g;
+	float b;
+};
+
+struct vertex {
+    float x;
+    float y;
+    float z;
+    float w;
+
+    vertex() {};
+    vertex(float x, float y) {
+        this->x = x;
+        this->y = y;
+    }
+    vertex(float x, float y, float z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    vertex(float x, float y, float z, float w) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->w = w;
+    }
+};
+
+struct context {
+    int width, height;
+    int index;
+    colorPixel clearColor;
+
+    float* colorBuffer;
+    
+    float* depthBuffer;
+    float vieportMatrix[4][4];
+
+    bool depthTest;
+    enum sglEMatrixMode matrixMode;
+    enum sglEElementType elementType;
+
+    colorPixel drawingColor;
+    float pointSize;
+
+    std::vector<std::array<float, 16>> modelViewMatricesStack;
+    std::vector<std::array<float, 16>> projectionMatricesStack;
+    std::vector<vertex*> vertexBuffer;
+
+    context(int width, int height, int index) {
+        this->width = width;
+        this->height = height;
+        this->index = index;
+        colorBuffer = (float*)malloc(width * height * 3 * sizeof(float));
+        depthBuffer = (float*)malloc(width * height * sizeof(float));
+        //TODO malloc vertexBuffer
+        depthTest = false;
+        clearColor.r = 0;
+        clearColor.g = 0;
+        clearColor.b = 0;
+    }
 };
 
 /// SGL variables
-int currentContext;
-std::vector<float**> modelViewMatricesStack;
-std::vector<float**> projectionMatricesStack;
-std::vector<colorPixel*> colorBuffer;
+
 bool sglBeginEndRunning = false;
+
+std::vector<context*> contexts;
+int contextCounter = 0;
+context* currentContext;
+std::array<float, 16> identity{ {1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0} };
 
 
 static inline void setErrCode(sglEErrorCode c) 
@@ -80,30 +141,44 @@ void sglInit(void) {
 }
 
 void sglFinish(void) {
+    for (int i = 0; i < contexts.size(); i++) {
+        delete contexts[i];
+    }
 
 }
 
 int sglCreateContext(int width, int height) {
-	
-	return 0;
+    context *thisContext = new context(width, height, contextCounter);
+    contexts.push_back(thisContext);
+    contextCounter++;
+	return thisContext->index;
 }
 
 void sglDestroyContext(int id) {
-
+    for (int i = 0; i < contexts.size(); i++) {
+        int cont = contexts[i]->index;
+        if (cont == id) {
+            delete contexts[i];
+            contexts.erase(contexts.begin() + id);
+        }
+    }
 }
 
 void sglSetContext(int id) {
-
+    for (int i = 0; i < contexts.size(); i++) {
+        if (id == contexts[i]->index) {
+            currentContext = contexts[i];
+            break;
+        }
+    }
 }
 
 int sglGetContext(void) {
-	
-	return 0;
+    return currentContext->index;
 }
 
 float *sglGetColorBufferPointer(void) {
-	
-	return 0;
+    return currentContext->colorBuffer;
 }
 
 //---------------------------------------------------------------------------
@@ -111,19 +186,34 @@ float *sglGetColorBufferPointer(void) {
 //---------------------------------------------------------------------------
 
 void sglClearColor(float r, float g, float b, float alpha) {
-
+    std::cout << currentContext->index << std::endl;
+    currentContext->clearColor.r = r;
+    currentContext->clearColor.g = g;
+    currentContext->clearColor.b = b;
 }
 
 void sglClear(unsigned what) {
-
+    if (what == SGL_COLOR_BUFFER_BIT) {
+        for (int i = 0; i < currentContext->width * currentContext->height; i += 3) {
+            currentContext->colorBuffer[i] = currentContext->clearColor.r;
+            currentContext->colorBuffer[i + 1] = currentContext->clearColor.g;
+            currentContext->colorBuffer[i + 2] = currentContext->clearColor.b;
+        }
+    }
+    else {
+        for (int i = 0; i < currentContext->width * currentContext->height; i += 1) {
+            currentContext->depthBuffer[i] = INFINITY;
+        }
+    }
 }
 
 void sglBegin(sglEElementType mode) {
-
+    sglBeginEndRunning = true;
+    currentContext->elementType = mode;
 }
 
 void sglEnd(void) {
-
+    sglBeginEndRunning = false;
 }
 
 void sglVertex4f(float x, float y, float z, float w) {
@@ -135,7 +225,7 @@ void sglVertex3f(float x, float y, float z) {
 }
 
 void sglVertex2f(float x, float y) {
-
+    currentContext->vertexBuffer.push_back(new vertex(x,y));
 }
 
 void sglCircle(float x, float y, float z, float radius) {
@@ -155,11 +245,11 @@ void sglArc(float x, float y, float z, float radius, float from, float to) {
 //---------------------------------------------------------------------------
 
 void sglMatrixMode(sglEMatrixMode mode) {
-
+    currentContext->matrixMode = mode;
 }
 
 void sglPushMatrix(void) {
-
+ 
 }
 
 void sglPopMatrix(void) {
@@ -167,7 +257,12 @@ void sglPopMatrix(void) {
 }
 
 void sglLoadIdentity(void) {
-
+    if (currentContext->matrixMode == SGL_PROJECTION) {
+        currentContext->projectionMatricesStack.push_back(identity);
+    }
+    if (currentContext->matrixMode == SGL_MODELVIEW) {
+        currentContext->modelViewMatricesStack.push_back(identity);
+    }
 }
 
 void sglLoadMatrix(const float *matrix) {
@@ -203,7 +298,22 @@ void sglFrustum(float left, float right, float bottom, float top, float near, fl
 }
 
 void sglViewport(int x, int y, int width, int height) {
-
+    currentContext->vieportMatrix[0][0] = width/2;
+    currentContext->vieportMatrix[0][1] = 0;
+    currentContext->vieportMatrix[0][2] = 0;
+    currentContext->vieportMatrix[0][3] = x+width/2;
+    currentContext->vieportMatrix[1][0] = 0;
+    currentContext->vieportMatrix[1][1] = height/2;
+    currentContext->vieportMatrix[1][2] = 0;
+    currentContext->vieportMatrix[1][3] = y + height / 2;
+    currentContext->vieportMatrix[2][0] = 0;
+    currentContext->vieportMatrix[2][1] = 0;
+    currentContext->vieportMatrix[2][2] = 1;
+    currentContext->vieportMatrix[2][3] = 0;
+    currentContext->vieportMatrix[3][0] = 0;
+    currentContext->vieportMatrix[3][1] = 0;
+    currentContext->vieportMatrix[3][2] = 0;
+    currentContext->vieportMatrix[3][3] = 1;
 }
 
 //---------------------------------------------------------------------------
@@ -211,6 +321,9 @@ void sglViewport(int x, int y, int width, int height) {
 //---------------------------------------------------------------------------
 
 void sglColor3f(float r, float g, float b) {
+    currentContext->drawingColor.r = r;
+    currentContext->drawingColor.g = g;
+    currentContext->drawingColor.b = b;
 
 }
 
@@ -219,15 +332,15 @@ void sglAreaMode(sglEAreaMode mode) {
 }
 
 void sglPointSize(float size) {
-
+    currentContext->pointSize = size;
 }
 
 void sglEnable(sglEEnableFlags cap) {
-
+    currentContext->depthTest = cap;
 }
 
 void sglDisable(sglEEnableFlags cap) {
-
+    currentContext->depthTest = cap;
 }
 
 //---------------------------------------------------------------------------
